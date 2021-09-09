aaa7730896ba3fad41bbc27845ec1c91a1b2c811
bmclaughlin3 <b4mclaughlin@student.bridgew.edu>
2021-09-09 09:48:50 -0400

    End of Tuesday

--- src/Ex_3.java
+++ src/Ex_3.java
@@ -2,16 +2,20 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ex_3 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         var studentNames = new ArrayList<String>();
         var reader = new Scanner(System.in);
         var userResponse = "";
-        while (!userResponse.equals("done")) {
+        while (!userResponse.equals("done")){
             System.out.print("Enter next name or 'done' to end:");
             userResponse = reader.nextLine();
-            if (userResponse.equals("done")) ;
-            studentNames.add(userResponse);
-
+            if (!userResponse.equals("done")){
+                studentNames.add(userResponse);
+            }
+        }
+        System.out.println("The students in your class are:");
+        for (var Name: studentNames){
+            System.out.println(Name);
         }
     }
 }
